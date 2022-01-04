6389cba334f7e9d8c84f6c8a0bc3657fffa36d4b
Mithras Chaudhry <46507768+reboseyekei@users.noreply.github.com>
2022-01-04 10:35:11 -0600

    Setting up template
    still need to set up themes

---
+++ src/pages/studio.js
@@ -0,0 +1,29 @@
+//General
+import React from 'react';
+
+//Material-UI
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+//Modules (Major only)
+
+//Contexts
+
+export default function Studio() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              R Blocks
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    )
+}
