1b5af7c0284cd6c013cd097f986fbb4f3a558738
Dhyey Mavani <ddmavani2003@gmail.com>
2022-06-17 11:04:17 -0400

    datacamp update

--- src/pages/modules/compiler.js
+++ src/pages/modules/compiler.js
@@ -10,8 +10,6 @@ import React from 'react';
 //Style
 import "./styles/base.css";
 
-var codeBody;
-
 
 export default function Sidebar() {
 
@@ -24,6 +22,7 @@ export default function Sidebar() {
                 </div>
                 
                 <div className="output" style={{ flex: 40, background: "#fff", boxShadow: "rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem, rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem"}}>
+                    
                 </div>
 
                 <div class="exercise">
@@ -45,6 +44,7 @@ export default function Sidebar() {
                     </div>
                     </div>
                 </div>
+                
 
 
 
@@ -55,11 +55,16 @@ export default function Sidebar() {
                 </div>
                 <div className="output" style={{ flex: 20 }}>
                     <div className="code">
-                        <div style={{ color: "white", flex: 4, background: "#1e1e1e", borderRadius: "2px" }}>
+                        <div style={{ color: "white", flex: 100, background: "#1e1e1e", borderRadius: "2px" }}>
                             <body id = "codeBody"> 
+                                <font size="+2">
                                 <p>
-                                    Code gets updated here!
-                                </p> 
+                                    # R Code gets updated here! <br></br>
+                                    library(mosaic) <br></br>
+                                    gf_bar(~substance, data = HELPrct) <br></br>
+                                    
+                                </p>
+                                </font> 
                             </body>
                         </div>
                         <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-around" }}>
@@ -84,4 +89,28 @@ export default function Sidebar() {
     )
 }
 
-codeBody = document.getElementById("codeBody");
+
+/*
+
+// This should be added in the complier element above in order to connect with the datacamp API
+
+<div class="exercise">
+    <div class="title">
+    <h2>How it works</h2>
+    </div>
+    <div data-datacamp-exercise data-lang="r" data-height="500">
+    <code data-type="pre-exercise-code"># no pec</code>
+    <code data-type="sample-code">
+        
+        3 + 4
+    </code>
+    
+    
+    <div data-type="hint">
+        <p>Just add a line of R code that calculates the sum of 6 and 12, just like the
+        example
+        in the sample code!</p>
+    </div>
+    </div>
+</div>
+*/
